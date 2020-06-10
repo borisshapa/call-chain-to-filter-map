912c018c97a65f82e4fd9072977444dc9debd5fe
Boris Shaposhnikov <bshaposhnikov01@gmail.com>
2020-06-10 19:03:13 +0300

    Delete package-info.java

--- src/call_chain_to_filter_map/expression/boolean_operators/comparison/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Comparison operations.
- *
- * @author <a href="https://teleg.run/borisshapa">Boris Shaposhnikov</a>
- */
-package call_chain_to_filter_map.expression.boolean_operators.comparison;
