0b351cb8be3ffe0ff89bcee5bf431e8564161a48
Boris Shaposhnikov <bshaposhnikov01@gmail.com>
2020-06-10 19:02:40 +0300

    Delete package-info.java

--- src/call_chain_to_filter_map/converter/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Function call converter to {@code <filter>%>%<map} form.
- *
- * @author <a href="https://teleg.run/borisshapa">Boris Shaposhnikov</a>
- */
-package call_chain_to_filter_map.converter;
