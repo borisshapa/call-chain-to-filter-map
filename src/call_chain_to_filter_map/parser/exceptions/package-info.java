ac72a2bed296a3c742e933c2551646e7d0227c3d
Boris Shaposhnikov <bshaposhnikov01@gmail.com>
2020-06-10 19:04:38 +0300

    Delete package-info.java

--- src/call_chain_to_filter_map/parser/exceptions/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Errors in parsing the language of operations on array elements.
- *
- * @author <a href="https://teleg.run/borisshapa">Boris Shaposhnikov</a>
- */
-package call_chain_to_filter_map.parser.exceptions;
